ebf328b2c121eefb716f0e67f12c2039bc9bfe9b
CalimotGrace <54784272+CalimotGrace@users.noreply.github.com>
2019-09-09 21:14:22 +0800

    Add files via upload

--- images/practice8.java
+++ images/practice8.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
-public class practice8 {
+public class practice8 
+{
+	public practice8()
+	{
+	
+	Scanner input = new Scanner(System.in);
+	
+	 System.out.print("Enter a degree in Celsius: ");
+     double celsius = input.nextDouble();
+     
+     //Compute
+     double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
+     System.out.println(celsius + " degree Celsius is equal to " + fahrenheit + " in Fahrenheit");
+ }
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		new practice8 ();
 
 	}
 
