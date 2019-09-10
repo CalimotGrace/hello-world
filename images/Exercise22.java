6f13e890f0f1d1222331ad4a5a35fceba6768d9d
CalimotGrace <54784272+CalimotGrace@users.noreply.github.com>
2019-09-10 09:57:24 +0800

    Add files via upload

---
+++ images/Exercise22.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Exercise22 
+{
+  public Exercise22()
+  {
+    Scanner in = new Scanner (System.in);
+    
+    System.out.println("Enter the Ten Integers: ");
+    int n1 ,n2 ,n3 ,n4 ,n5 ,n6 ,n7 ,n8 ,n9 ,n10;
+         
+        int sum1 = in.nextInt();
+        int sum2 = in.nextInt();
+        int sum3 = in.nextInt();
+        int sum4 = in.nextInt();
+        int sum5 = in.nextInt();
+        int sum6 = in.nextInt();
+        int sum7 = in.nextInt();
+        int sum8 = in.nextInt();
+        int sum9 = in.nextInt();
+        int sum10 = in.nextInt();
+        
+        //compute
+        int tn1 = sum1;
+        int tn2 = sum1 +sum2;
+        int tn3 = sum2 + sum3;
+        int tn4 = sum3 + sum4;
+        int tn5 = sum4 + sum5;
+        int tn6 = sum5 + sum6;
+        int tn7 = sum6 + sum7;
+        int tn8 = sum7 + sum8;
+        int tn9 = sum8 + sum9;
+        int tn10 = sum9 + sum10;
+        
+        System.out.println("Result: " + tsum");
+                
+                
+      
+        
+       
+     public static void main(String[] args)
+        {
+            new Exercise22();
+        }
+}
