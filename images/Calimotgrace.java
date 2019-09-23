9e1ec305f59c0d7658c1ec20e1d129785a7eb5d7
CalimotGrace <54784272+CalimotGrace@users.noreply.github.com>
2019-09-23 09:51:47 +0800

    Add files via upload

--- images/Calimotgrace.java
+++ images/Calimotgrace.java
@@ -1,72 +1,102 @@
+import static java.lang.System.in;
+import java.util.Random;
 import java.util.Scanner;
 
-public class Calimotgrace
-{
-  public Calimotgrace ()
-  {
-	  Scanner in = new Scanner(System.in);
-	  
-	//Declare variable
-	  int grade;
-	  String GM="";
-	  
-	//in1 
-      System.out.print("Input Grade: ");
-	  grade = in.nextInt();
-	  if (grade <= 95 && grade <= 100)
-	  GM = "EXCELLENT!";
-	  if (grade <= 85 && grade <= 94)
-	  GM = "GOOD!";
-	  if (grade <= 80 && grade <= 84)
-	  GM = "APPROACHING!";
-	  if (grade <= 75 && grade <= 79)
-	  GM = "NEED IMPROVEMENT";
-	  if (grade <= 74)
-	  GM = "Poor";
-	  
-  //out1
-	  System.out.printf(grade + " " + GM + "\n"); 
-	  
-  //in2 
-      System.out.print("Input Grade: ");
-	  grade = in.nextInt();
-	  if (grade <= 95 && grade <= 100)
-	  GM = "EXCELLENT!";
-	  if (grade <= 85 && grade <= 94)
-	  GM = "GOOD!";
-	  if (grade <= 80 && grade <= 84)
-	  GM = "APPROACHING!";
-	  if (grade <= 75 && grade <= 79)
-	  GM = "NEED IMPROVEMENT";
-	  if (grade <= 74)
-	  GM = "Poor";
-	  
-  //out2
-	  System.out.printf(grade + " " + GM + "\n");
-	 
-	  
-  //in3
-      System.out.print("Input Grade: ");
-	  grade = in.nextInt();
-	  if (grade <= 95 && grade <= 100)
-	  GM = "EXCELLENT!";
-	  if (grade <= 85 && grade <= 94)
-	  GM = "GOOD!";
-	  if (grade <= 80 && grade <= 84)
-	  GM = "APPROACHING!";
-	  if (grade <= 75 && grade <= 79)
-	  GM = "NEED IMPROVEMENT";
-	  if (grade <= 74)
-	  GM = "Poor";
-	  
-  //out3
-	  System.out.printf(grade + " " + GM + "\n");
-		 
-  }
-	public static void main(String[] args)
-	{
-		new Calimotgrace ();
+public class Calimotgrace {
 
-	}
-
+    public static void main(String[] args) {
+     
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input: ");
+        long numPhone, remainder;
+        long firstpart, secondpart, thirdpart;
+      
+        
+    System.out.println("Enter a 10-digit numbers: ");
+    numPhone = in.nextLong();
+    firstpart = numPhone/10000000;
+    remainder = numPhone%10000000;
+    secondpart = remainder/100000;
+    thirdpart = remainder%100000;
+    
+    if(firstpart == 800 || firstpart == 888 || firstpart == 877 || firstpart == 866)
+    {
+        System.out.println(firstpart + "-" + secondpart + "-" + thirdpart + "is a toll-free number ");
+    }
+    else
+    {
+        System.out.println(firstpart + "-"  + secondpart + "-" + thirdpart + " is a toll-free number " );
+    }
+        
+   
+        Random random = new Random ();
+       
+        int comp;
+        
+        comp= random.nextInt(3);
+        
+    //display
+        System.out.println("Rock: 0; Scissors: 1; Paper: 2 --");
+        System.out.println("Choose: ");
+        int player = in.nextInt();
+        System.out.println("The Computer Chooses " + comp);
+        System.out.println("*****************************");
+        
+        if (player == 0){
+            System.out.println("Player: Rock");
+        }
+        if (comp == 0){
+            System.out.println("Computer: Rock");
+        }
+        if (player == 1){
+            System.out.println("Player:Scissors");
+        }
+        if (comp == 1){
+            System.out.println("Computer:Scirssorrs");
+        }
+        if (player == 2){
+            System.out.println("Player: Paper");
+        }
+        if (comp == 2){
+            System.out.println("Computer: Paper");
+        }
+      //endif
+        if (player == 0 && comp == 0){
+            System.out.println("It's a tie!");
+        }
+        else if (player == 1 && comp == 1){
+            System.out.println("It's a tie!");
+        }
+        else if (player == 2 && comp == 2){
+            System.out.println("It's a tie!");
+        }
+        else if (player == 0 && comp == 1){
+            System.out.println ("Player Wins!");
+        }
+        else if (player == 1 && comp == 0){
+            System.out.println ("Computer Wins!");
+        }
+        else if (player == 0 && comp == 2){
+            System.out.println("Computer Wins!");
+        }
+        else if (player == 2 && comp == 0){
+            System.out.println("Player Wins!");
+        }
+        else if (player == 1 && comp== 0){
+            System.out.println("Computer Wins!");
+        }
+        else if (player == 1 && comp == 0){
+            System.out.println("Player Wins!");
+        }
+        else if (player == 0 && comp == 1){
+            System.out.println("Computer Wins!");
+        }
+        else if (player == 1 && comp == 2){
+            System.out.println("Player Wins!");
+        }
+        else if (player == 2 && comp == 1){
+            System.out.println("Computer Wins!");
+        }
+    }
+    
 }
